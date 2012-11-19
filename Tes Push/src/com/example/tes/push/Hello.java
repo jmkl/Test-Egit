d6107aea73e32768cd34bd19d0181cc8955da528
dcsms <jimikill3d@gmail.com>
2012-11-19 18:20:18 +0700

    update

--- Tes Push/src/com/example/tes/push/Hello.java
+++ Tes Push/src/com/example/tes/push/Hello.java
@@ -2,13 +2,18 @@ package com.example.tes.push;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.widget.Toast;
 
 public class Hello extends Activity {
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello);
+        ShowToast("Hello Gays");
+    }
+    private void ShowToast(String pesan){
+    	Toast.makeText(Hello.this, pesan, Toast.LENGTH_SHORT).show();
     }
 }
